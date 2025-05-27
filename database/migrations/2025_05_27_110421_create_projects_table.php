1acc1a484e80287fc745985ab363e6f829a2e4b2
zh-hadi <zhhadi50@gmail.com>
2025-05-27 18:01:11 +0600

    project  datatable create just

---
+++ database/migrations/2025_05_27_110421_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('client');
+            $table->string('publish_date')->nullable();
+            $table->string('url')->nullable();
+            $table->string('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
